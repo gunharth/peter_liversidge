0931597ef22b0179765ea2718ec99644b18bdbab
Gunharth Randolf <hello@gunharth.io>
2019-04-09 10:21:15 +0200

    tests

--- src/app.js
+++ src/app.js
@@ -30,57 +30,50 @@ $(function () {
         let outerLeftMargin = '';
         let outerRightMargin = '';
 
-        // if (landscapeLarge.includes(i)) style = 'landscapeLarge';
-        if (landscapeMediumLarge.includes(i)) style = 'landscapeMediumLarge';
-        if (landscapeMedium.includes(i)) style = 'landscapeMedium';
-        if (landscapeSmall.includes(i)) style = 'landscapeSmall';
-        if (landscapeSupersmall.includes(i)) style = 'landscapeSupersmall';
+        // if (landscapeMediumLarge.includes(i)) style = 'landscapeMediumLarge';
+        // if (landscapeMedium.includes(i)) style = 'landscapeMedium';
+        // if (landscapeSmall.includes(i)) style = 'landscapeSmall';
+        // if (landscapeSupersmall.includes(i)) style = 'landscapeSupersmall';
 
-        if (portraitLarge.includes(i)) style = 'portraitLarge';
-        if (portraitMedium.includes(i)) style = 'portraitMedium';
-        if (portraitMediumLarge.includes(i)) style = 'portraitMediumLarge';
-        if (portraitSmall.includes(i)) style = 'portraitSmall';
-        if (portraitSupersmall.includes(i)) style = 'portraitSupersmall';
+        // if (portraitLarge.includes(i)) style = 'portraitLarge';
+        // if (portraitMedium.includes(i)) style = 'portraitMedium';
+        // if (portraitMediumLarge.includes(i)) style = 'portraitMediumLarge';
+        // if (portraitSmall.includes(i)) style = 'portraitSmall';
+        // if (portraitSupersmall.includes(i)) style = 'portraitSupersmall';
 
-        if (overlapTop.includes(i)) style = 'overlapTop';
-        if (overlapTopSmall.includes(i)) style = 'overlapTopSmall';
-        if (overlapMiddle.includes(i)) style = 'overlapMiddle';
-        if (overlapMiddleSmall.includes(i)) style = 'overlapMiddleSmall';
-        if (overlapBottom.includes(i)) style = 'overlapBottom';
-        if (overlapBottomSmall.includes(i)) style = 'overlapBottomSmall';
+        // if (overlapTop.includes(i)) style = 'overlapTop';
+        // if (overlapTopSmall.includes(i)) style = 'overlapTopSmall';
+        // if (overlapMiddle.includes(i)) style = 'overlapMiddle';
+        // if (overlapMiddleSmall.includes(i)) style = 'overlapMiddleSmall';
+        // if (overlapBottom.includes(i)) style = 'overlapBottom';
+        // if (overlapBottomSmall.includes(i)) style = 'overlapBottomSmall';
 
-        // if (overlapLandscapeMedium.includes(i)) style = 'overlapLandscapeMedium';
-        if (overlapPortraitTop.includes(i)) style = 'overlapPortraitTop';
+        // if (overlapPortraitTop.includes(i)) style = 'overlapPortraitTop';
 
 
-        if (overlap25.includes(i)) style += ' overlap25';
-        if (overlap75.includes(i)) style += ' overlap75';
+        // if (overlap25.includes(i)) style += ' overlap25';
+        // if (overlap75.includes(i)) style += ' overlap75';
 
-        for(let j = 0; j <= 20; j++) {
-            let space = `left${j * 10}px`;
-            if (eval(space).includes(i)) style += ' ' + space;
-        }
-        // for (let j = 250; j <= 400; j=j+50) {
-        //     let space = `left${j}px`;
-        //     console.log('yes');
+        // for(let j = 0; j <= 20; j++) {
+        //     let space = `left${j * 10}px`;
         //     if (eval(space).includes(i)) style += ' ' + space;
         // }
-        if (left5px.includes(i)) style += ' left5px';
-        if (left700px.includes(i)) style += ' left700px';
-        if (right1000px.includes(i)) style += ' right1000px';
+        // if (left5px.includes(i)) style += ' left5px';
+        // if (left700px.includes(i)) style += ' left700px';
+        // if (right1000px.includes(i)) style += ' right1000px';
 
-        if (zIndex2.includes(i)) zIndex += 'zIndex2';
+        // if (zIndex2.includes(i)) zIndex += 'zIndex2';
 
-        for (let j = 0; j <= 20; j++) {
-            let space = `leftMargin${j * 10}px`;
-            if (eval(space).includes(i)) outerLeftMargin += ' ' + space;
-        }
-        for (let j = 0; j <= 20; j++) {
-            let space = `rightMargin${j * 10}px`;
-            if (eval(space).includes(i)) outerRightMargin += ' ' + space;
-        }
+        // for (let j = 0; j <= 20; j++) {
+        //     let space = `leftMargin${j * 10}px`;
+        //     if (eval(space).includes(i)) outerLeftMargin += ' ' + space;
+        // }
+        // for (let j = 0; j <= 20; j++) {
+        //     let space = `rightMargin${j * 10}px`;
+        //     if (eval(space).includes(i)) outerRightMargin += ' ' + space;
+        // }
 
-        cardSection.append(`<div class="card--content ${zIndex} ${outerLeftMargin} ${outerRightMargin}"${debug ? ` id="card-${i}"` : ''}><img data-src="${imageURL}/${String(i).padStart(3, '0')}.jpg" src="images/transparent.png" class="${style} lazyload" alt="">${debug ? `<div class="number">${i}</div>` : ''}</div> `);
+        cardSection.append(`<div class="card--content ${style} ${zIndex} ${outerLeftMargin} ${outerRightMargin}"${debug ? ` id="card-${i}"` : ''}><img data-src="${imageURL}/${String(i).padStart(3, '0')}.jpg" src="images/transparent.png" class="lazyload" alt="">${debug ? `<div class="number">${i}</div>` : ''}</div> `);
 
         // RESPONSIVE cardSection.append(`<div class="card--content">
         //                         <img
